024aefa9222fee05773c299c8955ef3088d23c4f
Karan <karansethia24@gmail.com>
2022-09-23 21:02:17 +0530

    Completing Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
+import './Header.css';
+import logo from '/Users/karansethia/Documents/React-Notes/app-files/src/media/notes.png';
 export default function Header() {
     return (
-        <div>
-            <img src="" alt=""/>
-            <h1>Your Notes</h1>
+        <div className='header'>
+            <img className="logo" src={logo} alt=""/>
+            <h1 className="title">YOUR NOTES</h1>
         </div>
     )
 }
