d3d6de39b1106e140f4c162cec71b4800e525105
Karan <karansethia24@gmail.com>
2022-09-26 21:13:40 +0530

    Adding Dummy data for the notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
+  const NOTES_DATA = [
+    {
+      'id': 1,
+      'title': "Freelance work",
+      'sub-heading': "this is sub",
+      'content': "This is the content for freelance work"
+    },
+    {
+      'id': 2,
+      'title': "Internship work",
+      'sub-heading': "this is sub",
+      'content': "This is the content for Internship work"
+    },
+    {
+      'id': 3,
+      'title': "University work",
+      'sub-heading': "this is sub",
+      'content': "This is the content for University work"
+    },
+    {
+      'id': 4,
+      'title': "Personal work",
+      'sub-heading': "this is sub",
+      'content': "This is the content for Personal work"
+    }
+  ]
   return (<div>
     <Sidebar />
     </div>
