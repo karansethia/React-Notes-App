5b098b923dbfd0348112925601bd386f93375771
Karan <karansethia24@gmail.com>
2022-10-10 21:44:08 +0530

    design changes in Header

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import Header from "./Header"
 export default function Sidebar(props){
     // console.log(props);
     return(<div>
-        <Header heading={props[0]}/>
+        <Header />
         <NotesList />
     </div>
     )
